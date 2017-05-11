660381e0c587f592461519b470ed748c7ad118f1
Koen Hufkens <khufkens@Pandora.local>
2017-05-11 21:56:35 +0200

    rename

---
+++ R/daymet_subset.r
@@ -0,0 +1,44 @@
+#' Returns an offset dataset with data running from offset DOY in year -1 to
+#' offset DOY in the current year. Two years of data (730 data layers) are
+#' required for this function to work.
+#'
+#' The output serves as input for further data processing and
+#' modelling efforts.
+#'
+#' @param data: rasterStack or rasterBrick of 730 layers (2 consecutive years)
+#' @param offset: offset of the time series in DOY (default = 264, sept 21)
+#' @keywords modelling, mean daily temperature
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' my_subset = daymet_subset(mystack, offset = 264)
+#' }
+
+
+# create subset of layers to calculate phenology model output on
+daymet_subset = function(data, offset = 264){
+
+  # sanity check
+  if(nlayers(data) != 730){
+    stop("Does not contain two years worth of data, check stack size...")
+  }
+
+  # make a number of assumptions on the data structure
+  # mainly there are 730 layers per above, which are ordered
+  # sequantially 1:365 (days) x 2, this avoids dealing with
+  # extracting info from layer names
+  layer_year = sort(rep(1:2,365))
+  layer_doy = rep(1:365,2)
+
+  # final layer selection
+  layer_selection = which((layer_year == 1 & layer_doy >= 244) | (layer_year == 2 & layer_doy < 244))
+
+  # subset the data and assign correct
+  # layer names, for clarity
+  s = subset(data, layer_selection)
+  names(s) = layer_doy[layer_selection]
+
+  # final subset for phenology modelling
+  return(s)
+}
---
+++ R/daymet_tmean.r
@@ -0,0 +1,67 @@
+#' Averages tmax and tmin Daymet tiles into
+#' a single mean daily temperature (tmean) tile
+#' for easy post processing and modelling.
+#'
+#' Outputs a geotiff using the same naming scheme
+#' as the original files. Optionally a raster object
+#' is returned to the current workspace.
+#'
+#' @param path: full path location of the daymet tiles
+#' @param tile: which tile to process
+#' @param year: which year to process
+#' @param internal: TRUE / FALSE (if FALSE, write the output to file)
+#' using the Daymet file format protocol.
+#' @keywords modelling, mean daily temperature
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' daymet_tmean(path = ".",
+#'              tile = 11935,
+#'              year = 2015)
+#' }
+
+daymet_tmean = function(path='.',
+                        tile = NULL,
+                        year = NULL,
+                        lat_lon = TRUE,
+                        internal = FALSE){
+
+  # reproject to lat-lon
+  latlon = CRS("+init=epsg:4326")
+
+  # exit on missing tile
+  if ( is.null(tile) | is.null(year) ) {
+    stop('No tile or year provided ...')
+  }
+
+  # list all files
+  tmin = sprintf('%s/tmin_%s_%s.nc',path, year, tile)
+  tmax = sprintf('%s/tmax_%s_%s.nc',path, year, tile)
+
+  # load everything into a raster stack
+  minmax_stack = stack(tmin, tmax)
+
+  # list layers
+  layers = rep(1:365,2)
+
+  # calculate layer mean, but back in tmean stack
+  tmean_stack = stackApply(minmax_stack, indices = layers, fun = mean)
+
+  # reproject to lat lon if required, this is the default
+  # as lat lon coordinates are needed in subsequent routines
+  # for some modelling efforts
+  if (lat_lon == TRUE){
+    tmean_stack = projectRaster(tmean_stack, crs = latlon)
+  }
+
+  # return all data to raster, either as a geotiff
+  # or as a local object
+  if (internal == FALSE){
+    writeRaster(tmean_stack,
+              sprintf('%s/%s_%s_tmean.tif', path, year, tile),
+              overwrite = TRUE)
+  } else {
+    return(tmean_stack)
+  }
+}
