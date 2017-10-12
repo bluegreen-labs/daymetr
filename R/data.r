facd8ad096017a8f325bedf0d56a2dbbc5a3d46c
Koen Hufkens <koen.hufkens@gmail.com>
2017-10-12 11:57:05 +0200

    allow lazy loading tile_outlines file

---
+++ R/data.r
@@ -0,0 +1,15 @@
+#' tile_outlines
+#'
+#' Large SpatialPolygonDataFrame containing the outlines of all the 
+#' Daymet tiles available as well as projection information. This data
+#' was converted from a shapefile as provided on the Daymet  main website.
+#' 
+#' @format SpatialPolygonDataFrame
+#' \describe{
+#'   \item{TileID}{tile ID number}
+#'   \item{XMin}{minimum longitude}
+#'   \item{XMax}{maximum longitude}
+#'   \item{YMin}{minimum latitude}
+#'   \item{YMax}{maximum latitude}
+#' }
+"tile_outlines"
