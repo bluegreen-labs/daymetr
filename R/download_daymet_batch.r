af51d359a254bb19a7e1e9426999d86b6eb733da
Koen Hufkens <koen.hufkens@gmail.com>
2017-10-26 15:33:16 +0200

    conversion to apply loop, outputs nested list

--- R/download_daymet_batch.r
+++ R/download_daymet_batch.r
@@ -2,11 +2,12 @@
 #'
 #' This function downloads DAYMET data for several single pixel
 #' location.
-#' @param file_location file with several site locations and coordinates
+#' @param batch_file file with several site locations and coordinates
 #' in a format site, latitude, longitude
 #' @param start start of the range of years over which to download data
 #' @param end end of the range of years over which to download data
 #' @param internal assign or FALSE, load data into workspace or save to disc
+#' @return a nested list or writes data to file
 #' @keywords DAYMET, climate data
 #' @export
 #' @examples
@@ -15,19 +16,20 @@
 #' download_daymet_batch(file_location = "yourlocations.csv")
 #' }
 
-download_daymet_batch <- function(file_location,
+download_daymet_batch <- function(batch_file,
                                   start = 1980,
                                   end = as.numeric(format(Sys.time(), "%Y"))-1,
-                                  internal = "assign"){
+                                  internal = TRUE){
 
   # read table with sites and coordinates
-  locations = utils::read.table(file_location,sep=',')
+  locations = utils::read.table(batch_file, sep=',')
 
-  # loop over all lines in the file
-  for (i in 1:dim(locations)[1]){
-    site = as.character(locations[i,1])
-    lat = as.numeric(locations[i,2])
-    lon = as.numeric(locations[i,3])
+  # loop over all lines in the file return
+  # nested list
+  apply(locations, 1, function(location){
+    site = as.character(location[1])
+    lat = as.numeric(location[2])
+    lon = as.numeric(location[3])
     try(download_daymet(
       site = site,
       lat = lat,
@@ -37,5 +39,5 @@ download_daymet_batch <- function(file_location,
       internal = internal
     ),
     silent = FALSE)
-  }
+  })
 }
