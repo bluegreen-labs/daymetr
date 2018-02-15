0398ff8badd797fd9048ab9bc75fc63d4bdf18ff
khufkens <koen.hufkens@gmail.com>
2018-02-15 19:16:13 +0100

    use test_check()

--- tests/run_tests.r
+++ tests/run_tests.r
@@ -1,6 +1,6 @@
 # Run all unit tests
 
 # This is a wrapper around all the tests specified
-# in the inst/tests directory. These tests are visible
+# in the tests/testthat directory. These tests are visible
 # to users and serve as small examples as well.
-testthat::test_package("daymetr")
+testthat::test_check("daymetr")
