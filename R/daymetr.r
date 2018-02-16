cc418e2420e037e25595e383b092469a16878709
Koen Hufkens <koen.hufkens@gmail.com>
2018-02-16 21:34:28 +0100

    table of content daymetr

---
+++ R/daymetr.r
@@ -0,0 +1,19 @@
+#' Interface to the 'Daymet' Web Services
+#' 
+#' Programmatic interface to the 'Daymet' web services (\url{https://daymet.ornl.gov}). Allows for easy downloads of Daymet climate data directly to your R workspace or your computer. Routines for both single pixel data downloads and gridded (netCDF) data are provided.
+#' 
+#' @author Koen Hufkens
+#' @section daymetr functions:
+#' \tabular{llllll}{
+#' \code{\link{download_daymet}}\cr
+#' \code{\link{download_daymet_batch}}\cr
+#' \code{\link{download_daymet_tiles}}\cr
+#' \code{\link{download_daymet_ncss}}\cr
+#' \code{\link{daymet_grid_tmean}}\cr
+#' \code{\link{daymet_grid_offset}}
+#' }
+#' 
+#' @docType package
+#' @name daymetr
+#' @keywords package
+NULL
