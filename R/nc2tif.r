d0203d594f181cac21b97df65bdfc71a56702faf
Koen Hufkens <koen.hufkens@gmail.com>
2018-02-21 21:48:40 +0100

    adding silence option

--- R/nc2tif.r
+++ R/nc2tif.r
@@ -12,6 +12,7 @@
 #' files will be written, or whether files will not be 
 #' written in the event that there is already a .tif of 
 #' that file. (default = NULL)
+#' @param silent limit verbose output (default = FALSE)
 #' @return Converted geotiff files of all netCDF data in the provided
 #' directory (path).
 #' @keywords Daymet, climate data, gridded data, netCDF, conversion
@@ -24,64 +25,81 @@
 #'  # the provided path to geotiff files. Existing
 #'  # files will be overwritten. If set to FALSE,
 #'  # files will not be overwritten.
-#'  nc2tif(path = "path_with_daymet_netcdf_files",
+#'  
+#'    # download the data
+#'    download_daymet_ncss(param = "tmin",
+#'                         frequency = "annual",
+#'                         path = tempdir(),
+#'                         silent = TRUE))
+#'  
+#'  # convert files from nc to tif
+#'  nc2tif(path = tempdir(),
 #'  overwrite = TRUE)
+#'  
+#'  # print converted files
+#'  print(list.files(tempdir(), "*.tif"))
 #'  
 #'}
 
 nc2tif <- function(path = ".",
-                    file = NULL,
-                    overwrite = FALSE){
+                   file = NULL,
+                   overwrite = FALSE,
+                   silent = FALSE){
 
-  #providing initial feedback
+  # providing initial feedback
   cat("nc2tif is working. Be patient, this may take a while...\n")
   
   if(is.null(file)){
-    #make a vector of all .nc files in the directory
+    # make a vector of all .nc files in the directory
     files <- list.files(path=path,
                         pattern="\\.nc$",
                         full.names=TRUE)
   }else{
-    #make a vector of specified files
+    # make a vector of specified files
     files <- file
   }
   
-  #removing written files from write list if overwrite=FALSE
+  # removing written files from write list if overwrite=FALSE
   if(!overwrite){
-    tifs <- list.files(path=path, pattern="\\.tif$", full.names=TRUE)
+    tifs <- list.files(path=path,
+                       pattern="\\.tif$",
+                       full.names=TRUE)
     files <- files[!tools::file_path_sans_ext(files) 
                    %in% tools::file_path_sans_ext(tifs)]
-    cat("\nSkipping",length(tifs),"existing files.")
+    # feedback
+    if (!silent){
+      cat("\nSkipping",length(tifs),"existing files.")
+    }
   }else{
     files <- files
   }
   
-  #load ncdf for raster functions
-  loadNamespace('ncdf4')
-  
-  #create progress tracker to provide feedback during writing
+  # create progress tracker to 
+  # provide feedback during writing
   k <- 0
   
-  #begin looping through files
+  # begin looping through files
   for(i in files){
     
-    #modify progress count
+    # modify progress count
     k <- k+1
     
-    #provide feedback
-    cat("\nWriting",tools::file_path_sans_ext(i),
-        "\nfile",k,"of",length(files),"\n")
-    
+    if (!silent){
+      # provide feedback
+      cat("\nWriting",tools::file_path_sans_ext(i),
+          "\nfile",k,"of",length(files),"\n")
+    }
+  
     if(!any(grep(pattern="annttl|annavg",i))){
-      #if i is daily or monthly data use raster::brick
+      # if i is daily or monthly data use raster::brick
       data <- suppressWarnings(raster::brick(i))
         
     }else{
-      #if i is annual summary data use raster::raster
+      # if i is annual summary data use raster::raster
       data <- suppressWarnings(raster::raster(i))
     }
 
-    #write the file
+    # write the file
     raster::writeRaster(data,
                         filename=tools::file_path_sans_ext(i),
                         format="GTiff",
