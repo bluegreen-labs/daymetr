2db20eca528c1c8c39f9e051dab2f5bf83685a60
Koen Hufkens <koen.hufkens@gmail.com>
2018-03-04 21:53:40 +0100

    add year range tests

--- tests/testthat/test_ncss_downloads.r
+++ tests/testthat/test_ncss_downloads.r
@@ -3,39 +3,46 @@
 # freeform gridded data download check
 test_that("freefrom gridded download (ncss) checks",{
   
-  # download the data
+  # download daily data
   df_daily = try(download_daymet_ncss(param = "tmin",
                                       frequency = "daily",
                                       path = tempdir(),
                                       silent = TRUE))
   
-  # download the data
-  df_monthly = try(download_daymet_ncss(param = "tmin",
+  # download monthly data, bad location
+  df_monthly_loc = try(download_daymet_ncss(location = c(34, -82, 33.75),
+                                        param = "tmin",
                                         frequency = "monthly",
                                         path = tempdir(),
                                         silent = TRUE))
 
-  # download the data
+  # download monthly data
+  df_monthly = try(download_daymet_ncss(param = "tmin",
+                                        frequency = "monthly",
+                                        path = tempdir(),
+                                        silent = TRUE))
+  
+  # download out of range checks for years (min)
   df_monthly_min_year = try(download_daymet_ncss(param = "tmin",
                                         frequency = "monthly",
                                         start = 1970,
                                         path = tempdir(),
                                         silent = TRUE))
   
-  # download the data
+  # download out of range checks for years (max)
   df_monthly_max_year = try(download_daymet_ncss(param = "tmin",
                                         frequency = "monthly",
                                         end = 2100,
                                         path = tempdir(),
                                         silent = TRUE))
   
-  # download the data
+  # download annual data
   df_annual = try(download_daymet_ncss(param = "tmin",
                                        frequency = "annual",
                                        path = tempdir(),
                                        silent = TRUE))
   
-  # download the data
+  # download annual data for all data types
   df_all_annual = try(download_daymet_ncss(param = "ALL",
                                            frequency = "annual",
                                            path = tempdir(),
@@ -49,6 +56,7 @@ test_that("freefrom gridded download (ncss) checks",{
   # see if any of the runs failed
   check = !inherits(df_daily, "try-error") &
           !inherits(df_monthly, "try-error") &
+          inherits(df_monthly_loc, "try-error") &
           !inherits(df_annual, "try-error") &
           !inherits(df_all_annual, "try-error") &
           !inherits(df_all_daily, "try-error") &
