3482364ab50713a199ce9c9f34a520e552e2949e
khufkens <koen.hufkens@gmail.com>
2021-05-07 15:36:53 +0200

    update tiles info

--- R/data.r
+++ R/data.r
@@ -1,6 +1,6 @@
 #' tile_outlines
 #'
-#' Large SpatialPolygonDataFrame containing the outlines of all the 
+#' Large simple feature collection containing the outlines of all the 
 #' Daymet tiles available as well as projection information. This data
 #' was converted from a shapefile as provided on the Daymet main website.
 #' 
