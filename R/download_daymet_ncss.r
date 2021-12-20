308a1acdd54e586cdcfa74eca3c0b09ab824c4b5
khufkens <koen.hufkens@gmail.com>
2021-12-20 09:04:52 +0100

    SSL fix disable, ORNLs responsibility

--- R/download_daymet_ncss.r
+++ R/download_daymet_ncss.r
@@ -63,11 +63,11 @@ download_daymet_ncss <- function(
     message("NOTE: data is stored in tempdir() ...")
   }
   
-  # set SSL verify config to ignore
+  # temp. fix set SSL verify config to ignore
   # service is non critical (no encryption needed)
   # assuming CA authority issues are ORNL issues
   # not a man in the middle attack
-  httr::set_config(httr::config(ssl_verifypeer = 0L))
+  # httr::set_config(httr::config(ssl_verifypeer = 0L))
   
   # remove capitals from frequency
   frequency <- tolower(frequency)
